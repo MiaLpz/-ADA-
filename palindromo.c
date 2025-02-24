ff934176e01c7d04d9d793c6776f918b3dbb6464
MiaLpz <vlopezp.75@gmail.com>
2025-02-23 22:01:19 -0600

    Add files via upload

---
+++ palindromo.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool esPalindromo(int x){                    
+    if (x < 0 || (x % 10 == 0 && x != 0)){
+        return false;
+    }
+
+    int revertido = 0;    
+    while (x > revertido){    
+        revertido = revertido * 10 + x % 10;       
+        x /= 10;      
+    }
+
+    return x == revertido || x == revertido / 10;       	
+} 
+
+int main (){
+    int numero = 121;
+
+    
+    printf("%cEs %d un palindromo? %s\n", 168, numero, esPalindromo(numero) ? "verdadero" : "falso");
+
+    return 0;
+}
+
